8428698ca2679f0297fd057ce099eb1e9668274b
sjsang <sangnt6604@gmail.com>
2025-12-05 19:30:46 +0700

    style: Google login button

--- src/components/auth/GoogleLoginButton.jsx
+++ src/components/auth/GoogleLoginButton.jsx
@@ -46,7 +46,15 @@ export default function GoogleLoginButton() {
 
     return (
         <div>
-            <div id="google-login-btn"></div>
+            <div id="google-login-btn" className="hidden"></div>
+
+            <div
+                onClick={() => { document.querySelector('#google-login-btn [role="button"]').click() }}
+                className="w-full flex justify-center items-center gap-3 border border-gray-300 px-4 py-2 rounded-lg hover:bg-gray-100 cursor-pointer"
+            >
+                <img src="/icons8-google-32.png" width={24} height={24} />
+                <p className="text-gray-700 font-medium">Đăng nhập với Google</p>
+            </div>
         </div>
     );
 }
